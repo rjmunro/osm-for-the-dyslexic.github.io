fb1e6d76e33228e80dc2da1fa8a9aacb86cf5753
develost.com <info@develost.com>
2015-10-09 16:21:24 +0200

    removed unused commented code

--- tiled-map-browser/js/gesture-manager.js
+++ tiled-map-browser/js/gesture-manager.js
@@ -2,58 +2,11 @@
     "use strict";
     
     var mapCanvas = null;
-    //var idCanvas = null;
     var onZoomFunction = null;
     var onIdentifyFunction = null;
     var onPanFunction = null;
     var deltaX = 0;
     var deltaY = 0;
-    
-    /**
-     * viewport height and width
-     * 
-     */
-    //function viewport() {
-    //    var e = window, a = 'inner';
-    //    if (!('innerWidth' in window )) {
-    //        a = 'client';
-    //        e = document.documentElement || document.body;
-    //    }
-    //    return { width : e[ a+'Width' ] , height : e[ a+'Height' ] };
-    //} 
-    
-    /**
-     * Resize map canvas and context to full viewport
-     */
-    //function resizeMapCanvasToFullScreen(){
-    //  mapCanvas.style.width = "" + (viewport().width) + "px";
-    //    mapCanvas.style.height = "" + (viewport().height) + "px";
-    //   var context = mapCanvas.getContext("2d");
-    //    context.canvas.width  = "" + (viewport().width) + "";
-    //    context.canvas.height = "" + (viewport().height) + "";
-        
-    //    idCanvas.style.width = "" + (viewport().width) + "px";
-    //    idCanvas.style.height = "" + (viewport().height) + "px";
-    //    var context2 = idCanvas.getContext("2d");
-    //    context2.canvas.width  = "" + (viewport().width) + "";
-    //    context2.canvas.height = "" + (viewport().height) + "";
-        
-    //    printMessageOnCanvas("Function: "+"resizeMapCanvasToFullScreen" + "\n" + Date());
-    //}
-    
-    //function eraseMapCanvas(){
-    //    var context = mapCanvas.getContext("2d");
-    //    context.clearRect(0, 0, mapCanvas.width, mapCanvas.height);
-    //}
-    
-    //function printMessageOnCanvas(message){
-    //    var lines = message.split("\n");
-    //    var context=mapCanvas.getContext("2d");
-    //    context.font="20px Arial";
-    //    for (var i = 0; i < lines.length; i++) {
-    //        context.fillText(lines[i],10,50+i*25);
-    //    } 
-    //}
     
     function onPan(ev){
         if (ev.isFirst) {
@@ -64,36 +17,19 @@
         var currDeltaY = ev.deltaY - deltaY;
         deltaX = ev.deltaX;
         deltaY = ev.deltaY;
-        
-        //eraseMapCanvas();
-        //printMessageOnCanvas("Function: "+"onPan(" + ev.type + ")\n" + "velocityX: " +ev.velocityX.toFixed(3) + " velocityY: " + ev.velocityY.toFixed(3) + "\n" + Date());
-        // deltaX, deltaY
         onPanFunction(currDeltaX,currDeltaY);
-        
-        //console.log("onPan: " + ev.type);
     }
-
-    //function onZoom(value){
-    //    //eraseMapCanvas();
-    //    //printMessageOnCanvas("Function: "+"onZoom(" + value +")\n" + Date());    
-    //    console.log("onZoom: " + value);
-    //}
     
     function onPress(ev){
         onIdentifyFunction(ev.pointers[0].clientX,ev.pointers[0].clientY);
-        //eraseMapCanvas();
-        //printMessageOnCanvas("Function: "+"onPress(" + ev.type+")\n"+"clientX: " + ev.pointers[0].clientX + " clientY: " + ev.pointers[0].clientY + "\n" + Date());
-        //console.log("onPress: " + ev.type);
     }
     
     function zoomOut(){
         onZoomFunction(-1);
-        //onZoom(-1);
     }
 
     function zoomIn(){
         onZoomFunction(1);
-        //onZoom(1);
     }
     
     function mouseWheelHandler(e) {
